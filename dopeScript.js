75ad1f7be591a338456c6cc378162f8e70c36adc
ThirsTea <coldness1305@yahoo.fr>
2020-05-25 22:35:06 +0200

    Added storage feature in the localstorage
    +Added duration calculation
    +Added scripts to retrieve data from LocalStorage
    
    //Todo => Find a way to store keypresses (maybe jQuery)

--- dopeScript.js
+++ dopeScript.js
@@ -1,9 +1,42 @@
 function validateInput() {
     var myInput = document.getElementById("myText").value;
     var myOutput = document.getElementById("myOutput");
-    //document.write("Hello World");
+
+    localStorage.setItem("data", myInput)
     if (myInput.length == 0)
         alert("Error Empty Input :c");
-    myOutput.innerHTML = myInput;
+    myOutput.innerHTML = localStorage.getItem("data");
     return true;
+}
+
+function duration(timestamps) {
+    var last = timestamps.pop();
+    var durations = [];
+
+    while (timestamps.length) {
+        var newTime = timestamps.pop();
+        durations.push(last - (newTime));
+    }
+    return durations.reverse();
+}
+
+function storeKey() {
+    var array = JSON.parse().localStorage.getItem("")
+}
+
+
+function initTimeline() {
+    var timeline = {timestamps:{}};
+    localStorage.setItem("timeline", JSON.stringify(timeline));
+}
+
+function getData() {
+    var times = JSON.parse(localStorage.getItem("timeline"));
+
+    for (var t in times) {
+        if (times.hasOwnProperty(t)) {
+            var val = times[t];
+            console.log(val);
+        }
+    }
 }
